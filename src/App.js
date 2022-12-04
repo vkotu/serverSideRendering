503fdfe014b5a4e1d1f7e8c83422010f9eb42891
vkotu <vkotu@paypal.com>
2022-12-03 21:15:05 -0800

    code splitting

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { render } from "react-dom";
+import { StrictMode, useState, lazy, Suspense } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import ThemeContext from "./ThemeContext";
+
+const Details = lazy(() => import('./Details'));
+const SearchParams = lazy(() => import('./SearchParams'));
+
+const App = () => {
+  const theme = useState("darkblue");
+  return (
+    <StrictMode>
+      <Suspense fallback={<h2>loading, be patient you weirdo</h2>}>
+      <ThemeContext.Provider value={theme}>
+        <BrowserRouter>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeContext.Provider>
+      </Suspense>
+    </StrictMode>
+  );
+};
+
+render(<App />, document.getElementById("root"));
